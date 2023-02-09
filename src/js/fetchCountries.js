1be5f37ead64a0d957369ffdd3dbcb49de2aa47d
Nataliia Kalynovska <112168059+TaleQ@users.noreply.github.com>
2023-02-09 01:39:49 +0100

    updates fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,6 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const fetchCountries = (event) => {
+const fetchCountries = event => {
   let name = event.target.value.trim();
   console.log(name);
   if (!(name === '')) {
@@ -14,20 +14,37 @@ const fetchCountries = (event) => {
         return response.json();
       })
       .then(data => {
-        console.log(data);
-        if (data.length > 10) {
-          Notify.info("Too many matches found. Please enter a more specific name.");
-        }
-        if (data.length >= 2 && data.length <= 10) {
-        }
+        const card = document.querySelector('.country-info');
+        const list = document.querySelector('.country-list');
         if (data.length === 1) {
-
+          let countryLanguages = data.flatMap(country => country.languages);
+          let cardMarkup = data
+            .map(country => {
+              let languagesNames = countryLanguages.map(language => language.name).join(', ');
+              return `<p class="country-info__name"><img src="${country.flags.svg}" width="60" height="50" alt="Flag"></img>${country.name}</p><ul class="country-info__list"><li class="country-info__item"><span class="title">Capital:</span>${country.capital}</li><li class="country-info__item"><span>Population:</span>${country.population}</li><li class="country-info__item"><span>Languages:</span>${languagesNames}</li></ul>`;
+            })
+            .join('');
+          list.innerHTML = "";
+          card.innerHTML = cardMarkup;
+        } else if (data.length >= 2 && data.length <= 10) {
+          let listMarkup = data
+            .map(country => {
+              return `<li class="country-list__item"><img src="${country.flags.svg}" width="40" height="30" alt="Flag"></img><span class="country-list__name">${country.name}</span></li>`;
+            })
+            .join('');
+          card.innerHTML = "";
+          list.innerHTML = listMarkup;
+        } else if (data.length > 10) {
+          Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
         }
       })
       .catch(error => {
         Notify.failure('Oops, there is no country with that name');
-      });
+      })
   }
 };
 
-export {fetchCountries}
+export { fetchCountries };
+
