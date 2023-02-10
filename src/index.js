cb0135092c0933603cd917c3645e5c7dc65bfa8a
Nataliia Kalynovska <112168059+TaleQ@users.noreply.github.com>
2023-02-10 12:28:22 +0100

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import './css/styles.css';
-import {fetchCountries} from './js/fetchCountries.js';
+import { fetchCountries } from './js/fetchCountries.js';
 import debounce from 'lodash.debounce';
 
 const searchBox = document.querySelector('#search-box');
-// const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 300;
 
 searchBox.addEventListener(
   'input',
-  debounce(fetchCountries, [wait = 300])
-);
+  _.debounce(fetchCountries, DEBOUNCE_DELAY))
