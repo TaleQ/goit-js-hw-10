32c7f66576a71a3c2cb024dc64e6778adcf1f6db
Nataliia Kalynovska <112168059+TaleQ@users.noreply.github.com>
2023-02-10 01:53:18 +0100

    updates fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,8 +2,9 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const fetchCountries = event => {
   let name = event.target.value.trim();
-  console.log(name);
-  if (!(name === '')) {
+  const card = document.querySelector('.country-info');
+  const list = document.querySelector('.country-list');
+  if (name !== '') {
     return fetch(
       `https://restcountries.com/v2/name/${name}?fields=name,capital,population,languages,flags`
     )
@@ -14,25 +15,25 @@ const fetchCountries = event => {
         return response.json();
       })
       .then(data => {
-        const card = document.querySelector('.country-info');
-        const list = document.querySelector('.country-list');
         if (data.length === 1) {
           let countryLanguages = data.flatMap(country => country.languages);
           let cardMarkup = data
             .map(country => {
-              let languagesNames = countryLanguages.map(language => language.name).join(', ');
-              return `<p class="country-info__name"><img src="${country.flags.svg}" width="60" height="50" alt="Flag"></img>${country.name}</p><ul class="country-info__list"><li class="country-info__item"><span class="title">Capital:</span>${country.capital}</li><li class="country-info__item"><span>Population:</span>${country.population}</li><li class="country-info__item"><span>Languages:</span>${languagesNames}</li></ul>`;
+              let languagesNames = countryLanguages
+                .map(language => language.name)
+                .join(', ');
+              return `<div class="country-info__box"><img src="${country.flags.svg}" width="60" height="40" alt="Flag"></img><p>${country.name}</p></div><ul class="country-info__list"><li><b>Capital:</b> ${country.capital}</li><li><b>Population:</b> ${country.population}</li><li><b>Languages:</b> ${languagesNames}</li></ul>`;
             })
             .join('');
-          list.innerHTML = "";
+          list.innerHTML = '';
           card.innerHTML = cardMarkup;
         } else if (data.length >= 2 && data.length <= 10) {
           let listMarkup = data
             .map(country => {
-              return `<li class="country-list__item"><img src="${country.flags.svg}" width="40" height="30" alt="Flag"></img><span class="country-list__name">${country.name}</span></li>`;
+              return `<li><img src="${country.flags.svg}" width="40" height="30" alt="Flag"></img><span>${country.name}</span></li>`;
             })
             .join('');
-          card.innerHTML = "";
+          card.innerHTML = '';
           list.innerHTML = listMarkup;
         } else if (data.length > 10) {
           Notify.info(
@@ -42,9 +43,12 @@ const fetchCountries = event => {
       })
       .catch(error => {
         Notify.failure('Oops, there is no country with that name');
-      })
+      });
+  }
+  if (name === '') {
+    list.innerHTML = '';
+    card.innerHTML = '';
   }
 };
 
 export { fetchCountries };
-
